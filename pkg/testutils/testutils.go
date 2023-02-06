e1571d684a423241282ef97cb12cba27a6aa6236
Omar Diab <omar_diab@mckinsey.com>
2023-02-06 18:57:36 +0000

    refactor: replace gnomock with testcontainers for db methods test

--- pkg/testutils/testutils.go
+++ pkg/testutils/testutils.go
@@ -1,32 +1,65 @@
 package testutils
 
 import (
+	"context"
 	"database/sql"
 	"embed"
 	"fmt"
+	"time"
 
 	"tmprldemo/pkg/database"
 
+	"github.com/docker/go-connections/nat"
 	_ "github.com/jackc/pgx/v4/stdlib"
-	"github.com/orlangure/gnomock"
-	"github.com/orlangure/gnomock/preset/postgres"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
 // MustNewPostgresInstance starts a container running postgres and returns the container and the db instance.
 // This function accepts a migrations argument which it executes against the db instance running inside the container.
-func MustNewPostgresInstance(dbName string, migrations embed.FS) (*gnomock.Container, *sql.DB) {
-	p := postgres.Preset(
-		postgres.WithDatabase(dbName),
-	)
+func MustNewPostgresInstance(ctx context.Context, dbName string, migrations embed.FS) (testcontainers.Container, *sql.DB) {
+	req := testcontainers.ContainerRequest{
+		Image: "postgres:14-alpine",
+		Env: map[string]string{
+			"POSTGRES_USER":     "postgres",
+			"POSTGRES_PASSWORD": "password",
+			"POSTGRES_DB":       dbName,
+		},
+		ExposedPorts: []string{"5432"},
+		Cmd:          []string{"postgres", "-c", "fsync=off"},
+		WaitingFor: wait.ForAll(
+			wait.ForLog("database system is ready to accept connections").WithOccurrence(2).WithStartupTimeout(5 * time.Second),
+		).WithDeadline(1 * time.Minute),
+	}
 
-	container, err := gnomock.Start(p)
+	postgresContainer, err := testcontainers.GenericContainer(
+		ctx,
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	host, err := postgresContainer.Host(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	natPort, err := nat.NewPort("tcp", "5432")
+	if err != nil {
+		panic(err)
+	}
+	port, err := postgresContainer.MappedPort(ctx, natPort)
 	if err != nil {
 		panic(err)
 	}
 
 	connectionString := fmt.Sprintf(
 		"postgres://%s:%s@%s:%d/%s?sslmode=disable",
-		"postgres", "password", container.Host, container.DefaultPort(), dbName,
+		"postgres", "password", host, port.Int(), dbName,
 	)
 
 	db, err := sql.Open("pgx", connectionString)
@@ -44,5 +77,5 @@ func MustNewPostgresInstance(dbName string, migrations embed.FS) (*gnomock.Conta
 		panic(err)
 	}
 
-	return container, db
+	return postgresContainer, db
 }
