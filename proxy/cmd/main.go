f18cf51c0326e2f9b8ff0d04616880b9ae202214
Omar-V2 <omardiab4@googlemail.com>
2023-02-11 19:57:01 +0000

    refactor: use cleanenv for grpc-gateway

--- proxy/cmd/main.go
+++ proxy/cmd/main.go
@@ -8,14 +8,15 @@ import (
 	customerpb "tmprldemo/internal/pb/customer/v1"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/ilyakaznacheev/cleanenv"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-const (
-	gRPCCustomerServiceAddress = "customer-service:8080"
-	gRPCGatewayAddress         = "0.0.0.0:8081"
-)
+type Config struct {
+	CustomerServiceAddress string `env:"CUSTOMER_SERVICE_ADDRESS" env-default:"customer-service:8080"`
+	GatewayAddress         string `env:"GATEWAY_ADDRESS" env-default:"0.0.0.0:8081"`
+}
 
 func main() {
 	if err := run(); err != nil {
@@ -24,20 +25,25 @@ func main() {
 }
 
 func run() error {
+	var cfg Config
+
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		return fmt.Errorf("failed to read config from environment variables: %w", err)
+	}
 	mux := runtime.NewServeMux()
 
 	err := customerpb.RegisterCustomerServiceHandlerFromEndpoint(
 		context.Background(),
 		mux,
-		gRPCCustomerServiceAddress,
+		cfg.CustomerServiceAddress,
 		[]grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())},
 	)
 	if err != nil {
 		return fmt.Errorf("failed to serve gRPC gateway: %w", err)
 	}
 
-	log.Printf("gRPC gateway server listening on %s", gRPCGatewayAddress)
-	http.ListenAndServe(gRPCGatewayAddress, mux)
+	log.Printf("gRPC gateway server listening on %s", cfg.GatewayAddress)
+	http.ListenAndServe(cfg.GatewayAddress, mux)
 
 	return nil
 }
