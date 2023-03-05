1c9bdd36127e43b010ffbd56a83e8e33abe3c00b
Omar-V2 <omardiab4@googlemail.com>
2023-03-05 14:27:23 +0000

    cleanup: fix workflow tests

--- internal/customer/workflows/verifyphone/workflow_test.go
+++ internal/customer/workflows/verifyphone/workflow_test.go
@@ -1,26 +1,31 @@
 package verifyphone
 
 import (
+	"context"
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/testsuite"
 )
 
 type VerifyPhoneWorkflowTestSuite struct {
 	suite.Suite
 	testsuite.WorkflowTestSuite
-	env           *testsuite.TestWorkflowEnvironment
-	mockSmsSender *MockSMSSender
-	smsSender     SMSSender
+	env        *testsuite.TestWorkflowEnvironment
+	activities *activities
+}
+
+func TestVerifyPhoneWorkflow(t *testing.T) {
+	suite.Run(t, new(VerifyPhoneWorkflowTestSuite))
 }
 
 func (s *VerifyPhoneWorkflowTestSuite) SetupTest() {
 	s.env = s.NewTestWorkflowEnvironment()
-	s.mockSmsSender = &MockSMSSender{}
-	s.smsSender = SMSSender{Sender: s.mockSmsSender}
+	s.activities = &activities{}
 }
 
 func (s *VerifyPhoneWorkflowTestSuite) TearDownTest(suiteName, testName string) {
@@ -28,17 +33,27 @@ func (s *VerifyPhoneWorkflowTestSuite) TearDownTest(suiteName, testName string) 
 }
 
 func (s *VerifyPhoneWorkflowTestSuite) TestVerifyPhoneWorkflow() {
+	testCustomerID := uuid.NewString()
 	testPhoneNumber := "012345678"
 	testMessage := "Thanks for signing up to GoCoffee. Please enter the following code in our app to verify your phone number: 1234"
 
-	s.env.OnActivity(s.smsSender.SendSMS, mock.Anything, mock.Anything).
-		Return(
-			func(phoneNumber, message string) error {
-				s.Equal(testPhoneNumber, phoneNumber)
-				s.Equal(testMessage, message)
-				return nil
-			},
-		)
+	s.env.OnActivity(s.activities.SendSMS, mock.Anything, mock.Anything).Return(
+		func(phoneNumber, message string) error {
+			s.Equal(testPhoneNumber, phoneNumber)
+			s.Equal(testMessage, message)
+			return nil
+		})
+
+	s.env.OnActivity(s.activities.VerifyCustomer, mock.Anything, mock.Anything).Return(
+		func(ctx context.Context, customerID string) error {
+			s.Equal(testCustomerID, customerID)
+			return nil
+		})
+
+	// send the correct code on the first try - expecting workflow to complete thereafter.
+	s.env.RegisterDelayedCallback(func() {
+		s.env.SignalWorkflow(UserCodeSignal, "1234")
+	}, time.Minute*1)
 
 	params := WorkflowParams{
 		PhoneNumber:          testPhoneNumber,
@@ -46,35 +61,57 @@ func (s *VerifyPhoneWorkflowTestSuite) TestVerifyPhoneWorkflow() {
 		CodeValidityDuration: time.Minute * 2,
 	}
 
-	// send the correct code on the first try - expecting workflow to complete thereafter.
-	s.env.RegisterDelayedCallback(func() {
-		s.env.SignalWorkflow(UserCodeSignal, "1234")
-	}, time.Minute*1)
-
+	s.env.SetStartWorkflowOptions(client.StartWorkflowOptions{ID: testCustomerID})
 	s.env.ExecuteWorkflow(NewWorkflow, params)
 	s.True(s.env.IsWorkflowCompleted())
 
-	res, err := s.env.QueryWorkflow(VerificationStateQueryType)
+	res, err := s.env.QueryWorkflow(VerificationResultQueryType)
 	s.NoError(err)
 
-	var state VerificationState
-	err = res.Get(&state)
+	var result VerificationResult
+	err = res.Get(&result)
 	s.NoError(err)
-	s.Equal(state, CorrectCode)
+	s.Equal(CorrectCode, result)
 }
 
 func (s *VerifyPhoneWorkflowTestSuite) TestVerifyPhoneWorkflowAllowsMultipleTries() {
+	testCustomerID := uuid.NewString()
 	testPhoneNumber := "012345678"
 	testMessage := "Thanks for signing up to GoCoffee. Please enter the following code in our app to verify your phone number: 1234"
 
-	s.env.OnActivity(s.smsSender.SendSMS, mock.Anything, mock.Anything).
-		Return(
-			func(phoneNumber, message string) error {
-				s.Equal(testPhoneNumber, phoneNumber)
-				s.Equal(testMessage, message)
-				return nil
-			},
-		).Twice()
+	s.env.OnActivity(s.activities.SendSMS, mock.Anything, mock.Anything).Return(
+		func(phoneNumber, message string) error {
+			s.Equal(testPhoneNumber, phoneNumber)
+			s.Equal(testMessage, message)
+			return nil
+		}).Twice()
+
+	s.env.OnActivity(s.activities.VerifyCustomer, mock.Anything, mock.Anything).Return(
+		func(ctx context.Context, customerID string) error {
+			s.Equal(testCustomerID, customerID)
+			return nil
+		})
+
+	// send the incorrect code on the first try
+	s.env.RegisterDelayedCallback(func() {
+		s.env.SignalWorkflow(UserCodeSignal, "2345")
+	}, time.Minute*1)
+
+	// query the workflow for the result of the most recent attempt
+	s.env.RegisterDelayedCallback(func() {
+		res, err := s.env.QueryWorkflow(VerificationResultQueryType)
+		s.NoError(err)
+
+		var result VerificationResult
+		err = res.Get(&result)
+		s.Require().NoError(err)
+		s.Equal(IncorrectCode, result)
+	}, time.Minute*1+time.Second*10)
+
+	// send the correct code on the second try
+	s.env.RegisterDelayedCallback(func() {
+		s.env.SignalWorkflow(UserCodeSignal, "1234")
+	}, time.Minute*2)
 
 	params := WorkflowParams{
 		PhoneNumber:          testPhoneNumber,
@@ -82,40 +119,29 @@ func (s *VerifyPhoneWorkflowTestSuite) TestVerifyPhoneWorkflowAllowsMultipleTrie
 		CodeValidityDuration: time.Minute * 3,
 	}
 
-	// send the incorrect code on the first try
-	s.env.RegisterDelayedCallback(func() {
-		s.env.SignalWorkflow(UserCodeSignal, "2345")
-	}, time.Minute*1)
-
-	// send the correct code on the second try
-	s.env.RegisterDelayedCallback(func() {
-		s.env.SignalWorkflow(UserCodeSignal, "1234")
-	}, time.Minute*2)
-
+	s.env.SetStartWorkflowOptions(client.StartWorkflowOptions{ID: testCustomerID})
 	s.env.ExecuteWorkflow(NewWorkflow, params)
 	s.True(s.env.IsWorkflowCompleted())
 
-	res, err := s.env.QueryWorkflow(VerificationStateQueryType)
+	res, err := s.env.QueryWorkflow(VerificationResultQueryType)
 	s.NoError(err)
 
-	var state VerificationState
-	err = res.Get(&state)
+	var result VerificationResult
+	err = res.Get(&result)
 	s.NoError(err)
-	s.Equal(state, CorrectCode)
+	s.Equal(CorrectCode, result)
 }
 
-func (s *VerifyPhoneWorkflowTestSuite) TestVerifyPhoneWorkflowHasErrorStateOnMaximumAttempts() {
+func (s *VerifyPhoneWorkflowTestSuite) TestVerifyPhoneWorkflowMaximumAttemptsReached() {
 	testPhoneNumber := "012345678"
 	testMessage := "Thanks for signing up to GoCoffee. Please enter the following code in our app to verify your phone number: 1234"
 
-	s.env.OnActivity(s.smsSender.SendSMS, mock.Anything, mock.Anything).
-		Return(
-			func(phoneNumber, message string) error {
-				s.Equal(testPhoneNumber, phoneNumber)
-				s.Equal(testMessage, message)
-				return nil
-			},
-		)
+	s.env.OnActivity(s.activities.SendSMS, mock.Anything, mock.Anything).Return(
+		func(phoneNumber, message string) error {
+			s.Equal(testPhoneNumber, phoneNumber)
+			s.Equal(testMessage, message)
+			return nil
+		}).Twice()
 
 	params := WorkflowParams{
 		PhoneNumber:          testPhoneNumber,
@@ -130,36 +156,41 @@ func (s *VerifyPhoneWorkflowTestSuite) TestVerifyPhoneWorkflowHasErrorStateOnMax
 
 	s.env.RegisterDelayedCallback(func() {
 		s.env.SignalWorkflow(UserCodeSignal, "4567")
-	}, time.Minute*1)
+	}, time.Minute*2)
 
 	s.env.ExecuteWorkflow(NewWorkflow, params)
 	s.True(s.env.IsWorkflowCompleted())
 
-	res, err := s.env.QueryWorkflow(VerificationStateQueryType)
+	res, err := s.env.QueryWorkflow(VerificationResultQueryType)
 	s.NoError(err)
 
-	var state VerificationState
-	err = res.Get(&state)
+	var result VerificationResult
+	err = res.Get(&result)
 	s.NoError(err)
-	s.Equal(state, MaxAttemptsReached)
+	s.Equal(MaxAttemptsReached, result)
 }
 
 func (s *VerifyPhoneWorkflowTestSuite) TestVerifyPhoneWorkflowCodeExpiration() {
+	testCustomerID := uuid.NewString()
 	testPhoneNumber := "012345678"
 	testMessage := "Thanks for signing up to GoCoffee. Please enter the following code in our app to verify your phone number: 1234"
 
-	s.env.OnActivity(s.smsSender.SendSMS, mock.Anything, mock.Anything).
-		Return(
-			func(phoneNumber, message string) error {
-				s.Equal(testPhoneNumber, phoneNumber)
-				s.Equal(testMessage, message)
-				return nil
-			},
-		)
+	s.env.OnActivity(s.activities.SendSMS, mock.Anything, mock.Anything).Return(
+		func(phoneNumber, message string) error {
+			s.Equal(testPhoneNumber, phoneNumber)
+			s.Equal(testMessage, message)
+			return nil
+		}).Twice()
+
+	s.env.OnActivity(s.activities.VerifyCustomer, mock.Anything, mock.Anything).Return(
+		func(ctx context.Context, customerID string) error {
+			s.Equal(testCustomerID, customerID)
+			return nil
+		})
 
 	params := WorkflowParams{
 		PhoneNumber:          testPhoneNumber,
-		MaximumAttempts:      1,
+		MaximumAttempts:      2,
 		CodeValidityDuration: time.Minute * 1,
 	}
 
@@ -168,18 +199,31 @@ func (s *VerifyPhoneWorkflowTestSuite) TestVerifyPhoneWorkflowCodeExpiration() {
 		s.env.SignalWorkflow(UserCodeSignal, "1234")
 	}, time.Minute*2)
 
+	// query the workflow for the result of the most recent attempt
+	s.env.RegisterDelayedCallback(func() {
+		res, err := s.env.QueryWorkflow(VerificationResultQueryType)
+		s.NoError(err)
+
+		var result VerificationResult
+		err = res.Get(&result)
+		s.Require().NoError(err)
+		s.Equal(CodeExpired, result)
+	}, time.Minute*2+time.Second*10)
+
+	// send the correct code before expiry to complete the workflow.
+	s.env.RegisterDelayedCallback(func() {
+		s.env.SignalWorkflow(UserCodeSignal, "1234")
+	}, time.Minute*3)
+
+	s.env.SetStartWorkflowOptions(client.StartWorkflowOptions{ID: testCustomerID})
 	s.env.ExecuteWorkflow(NewWorkflow, params)
 	s.True(s.env.IsWorkflowCompleted())
 
-	res, err := s.env.QueryWorkflow(VerificationStateQueryType)
+	res, err := s.env.QueryWorkflow(VerificationResultQueryType)
 	s.NoError(err)
 
-	var state VerificationState
-	err = res.Get(&state)
+	var result VerificationResult
+	err = res.Get(&result)
 	s.NoError(err)
-	s.Equal(state, MaxAttemptsReached)
-}
-
-func TestVerifyPhoneWorkflow(t *testing.T) {
-	suite.Run(t, new(VerifyPhoneWorkflowTestSuite))
+	s.Equal(CorrectCode, result)
 }
