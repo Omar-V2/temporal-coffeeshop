18f66839a493edb31ea58063f1c0594136a6a8df
Omar-V2 <omardiab4@googlemail.com>
2023-03-05 19:40:45 +0000

    chore: move sms code to own file

--- internal/customer/workflows/verifyphone/activity.go
+++ internal/customer/workflows/verifyphone/activity.go
@@ -2,7 +2,6 @@ package verifyphone
 
 import (
 	"context"
-	"fmt"
 )
 
 // activities defines the activity methods required for the verify phone workflow.
@@ -32,39 +31,4 @@ type CustomerVerifier interface {
 // Sender defines a method to send a text message via SMS to given phone number.
 type Sender interface {
 	SendMessage(phoneNumber, message string) error
-}
-
-type SMSSender struct {
-	Sender Sender
-}
-
-func (s *SMSSender) SendSMS(phoneNumber, message string) error {
-	return s.Sender.SendMessage(phoneNumber, message)
-}
-
-// TwilioSMSSender implements the SendMessage interface using the Twilio API
-// to send an SMS message to the provided phone number.
-type TwilioSMSSender struct{}
-
-func (t *TwilioSMSSender) SendMessage(phoneNumber, message string) error {
-	return nil
-}
-
-// MockSMSSender implements the SendMessage interface but simply prints the message to stdout.
-type MockSMSSender struct{}
-
-func (m *MockSMSSender) SendMessage(phoneNumber, message string) error {
-	fmt.Printf("sent message: %s to phone number : %s", message, phoneNumber)
-	return nil
-}
-
-// FaultySMSSender implements the SendMessage interface but always returns an error
-// when attempting to send a message to simulate a failure scenario.
-type FaultySMSSender struct{}
-
-func (m *FaultySMSSender) SendMessage(phoneNumber, message string) error {
-	return fmt.Errorf(
-		"failed sendint text message: %s to phone number %s",
-		message, phoneNumber,
-	)
 }
